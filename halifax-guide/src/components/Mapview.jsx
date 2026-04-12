03132967f8c7440b426ba8614bc987a33e5f52e8
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-04-12 15:34:55 -0300

    adding neighbourhoods and colored polygons

--- halifax-guide/src/components/Mapview.jsx
+++ halifax-guide/src/components/Mapview.jsx
@@ -1,15 +1,31 @@
-import {MapContainer,TileLayer} from 'react-leaflet'
+import {MapContainer,Polygon,TileLayer, Tooltip} from 'react-leaflet'
+import neighbourhoods from '../data/neighbourhoods'
+
 
 const HALIFAX_CENTER =  [44.6488,-63.5752]
 
-function Mapview(){
+function Mapview({onSelectNeighbourhood}){
     return(
         <MapContainer center={HALIFAX_CENTER} 
             zoom={13}
             style={{height:'100%',width:'100%'}}>
                 <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 attribution='0 OpenStreetMap contributors'/>
-            </MapContainer>
+
+        {neighbourhoods.map((n)=>(
+            <Polygon
+                key={n.id}
+                positions={n.coords}
+                pathOptions={{color:n.color,fillOpacity:0.3}}
+                eventHandlers={{
+                    click : () => onSelectNeighbourhood(n)
+                }}
+            >
+                <Tooltip>{n.name}</Tooltip>
+            </Polygon>
+        ))}
+
+        </MapContainer>
 
     )
 }
---
+++ halifax-guide/src/data/neighbourhoods.js
@@ -0,0 +1,64 @@
+const neighbourhoods = [
+  {
+    id: 1,
+    name: "North End",
+    color: "#4A90D9",
+    vibe: "Artsy, trendy, independent shops",
+    coords: [
+      [44.663, -63.598],
+      [44.663, -63.580],
+      [44.652, -63.580],
+      [44.652, -63.598],
+    ]
+  },
+  {
+    id: 2,
+    name: "Downtown Halifax",
+    color: "#E67E22",
+    vibe: "Business, nightlife, waterfront",
+    coords: [
+      [44.652, -63.580],
+      [44.652, -63.565],
+      [44.642, -63.565],
+      [44.642, -63.580],
+    ]
+  },
+  {
+    id: 3,
+    name: "Hydrostone",
+    color: "#27AE60",
+    vibe: "Historic, quiet, family-friendly",
+    coords: [
+      [44.668, -63.598],
+      [44.668, -63.585],
+      [44.663, -63.585],
+      [44.663, -63.598],
+    ]
+  },
+  {
+    id: 4,
+    name: "Quinpool",
+    color: "#8E44AD",
+    vibe: "Restaurants, students, walkable",
+    coords: [
+      [44.650, -63.608],
+      [44.650, -63.595],
+      [44.642, -63.595],
+      [44.642, -63.608],
+    ]
+  },
+  {
+    id:5,
+    name:"South End",
+    color:'#C0392B',
+    vide:"Universities,parks,quiet streets",
+    coords:[
+        [44.642,-63.580],
+        [44.642,-63.565],
+        [44.630,-63.565],
+        [44.630,-63.580],
+    ]
+  }
+]
+
+export default neighbourhoods
