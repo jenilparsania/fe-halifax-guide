c6da8796bd551fcd4d8dbb59b8a9fec40bbe3a0e
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-04-15 19:37:23 -0300

    adding Links and Routing to Sidebar and App.jsx

--- halifax-guide/src/App.jsx
+++ halifax-guide/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Mapview from './components/Mapview'
 import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Neighbourhooddetail from './pages/Neighbourhooddetail'
 
 function App() {
   const [selected,setSelected] = useState(null)
   return(
-    <div className='app'>
-      <Sidebar neighbourhood={selected} onClose={()=>setSelected(null)}/>
-      <Mapview onSelectNeighbourhood={setSelected}/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/neighbourhood/:slug' element={<Neighbourhooddetail/>}/>
+    </Routes>
   )
 }
 
--- halifax-guide/src/components/Sidebar.jsx
+++ halifax-guide/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './Sidebar.css'
 
 function Sidebar({neighbourhood,onClose}){
@@ -10,12 +11,16 @@ function Sidebar({neighbourhood,onClose}){
         )
     }
 
+    const slug = neighbourhood.name.toLowerCase().replace(/s+/g,'-')
     return(
         <div className='sidebar'>
             <button className="sidebar_close" onClick={onClose}>X</button>
             <div className='sidebar_color-bar' style={{background : neighbourhood.color}}/>
             <h2 className="sidebar_name">{neighbourhood.name}</h2>
             <p className="sidebar_vibe">{neighbourhood.vibe}</p>
+            <Link to={`neighbourhood/${slug}`} className='sidebar_link'>
+                Visit full Profile
+            </Link>
         </div>
 
     )
