bb5cceb5d44b6e3213b5da61cb3a7518c305717c
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-04-28 16:32:54 -0300

    filtering places

--- halifax-guide/src/pages/Home.jsx
+++ halifax-guide/src/pages/Home.jsx
@@ -2,13 +2,32 @@ import { useState } from 'react'
 import '../App.css'
 import Mapview from '../components/Mapview'
 import Sidebar from '../components/Sidebar'
+import places from '../data/places'
 
 function Home() {
   const [selected,setSelected] = useState(null)
+  const [activeCategory, setActiveCategory] = useState('all')
+  const [searchQuery , setSearchQuery] = useState('')
+
+  const filteredPlaces = places.filter((place)=> {
+    const matchesCategory = activeCategory === 'all' || place.category === activeCategory
+
+    const matchesSearch = place.name.toLowerCase().includes(searchQuery.toLowerCase())
+
+    return matchesCategory && matchesSearch
+
+  })
+
+
   return(
     <div className='app'>
       <Sidebar neighbourhood={selected} onClose={()=>setSelected(null)}/>
-      <Mapview onSelectNeighbourhood={setSelected}/>
+      <div style={{flex:1, display: 'flex' , flexDirection: 'column'}}> 
+        <Mapview onSelectNeighbourhood={setSelected} places= {filteredPlaces}/>
+        
+      </div>
+
+      
     </div>
   )
 }
