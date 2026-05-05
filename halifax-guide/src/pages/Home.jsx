c691b4c96598c3e6c0070ee8d47cecfb03966c0f
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-05-04 21:24:30 -0300

    connecting fe to be

--- halifax-guide/src/pages/Home.jsx
+++ halifax-guide/src/pages/Home.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../App.css'
 import Mapview from '../components/Mapview'
 import Sidebar from '../components/Sidebar'
-import places from '../data/places'
 import Filterbar from '../components/Filterbar'
+
+const API_URL = 'http://localhost:5000'
 
 function Home() {
   const [selected,setSelected] = useState(null)
   const [activeCategory, setActiveCategory] = useState('all')
   const [searchQuery , setSearchQuery] = useState('')
+  const [places,setPlaces] = useState([])
+  const [loading,setLoading] = useState(true) 
+
+  useEffect(()=>{
+    setLoading(true)
+    const url = activeCategory === 'all' ? 
+    `${API_URL}/api/places` 
+    : `${API_URL}/api/places?category=${activeCategory}`
+
+    fetch(url).then((res) => res.json())
+    .then((data)=>{
+      setPlaces(data)
+      setLoading(false)
+    })
+    .catch((err)=>{
+      console.error('Failed to fetch places : ',err)
+      setLoading(false)
+
+    })
+
+  },[activeCategory])
 
   const filteredPlaces = places.filter((place)=> {
     const matchesCategory = activeCategory === 'all' || place.category === activeCategory
