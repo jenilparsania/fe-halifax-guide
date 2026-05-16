c6ac0f50084f4e1a7f585953c97ae9da0556b06e
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2026-05-16 17:03:43 -0300

    adding visible class to Sidebar.jsx

--- halifax-guide/src/components/Sidebar.jsx
+++ halifax-guide/src/components/Sidebar.jsx
@@ -2,9 +2,19 @@ import { Link } from 'react-router-dom'
 import './Sidebar.css'
 
 function Sidebar({neighbourhood,onClose}){
+
+    const slug = neighbourhood
+    ? neighbourhood.name.toLowerCase().replace(/\s+/g,'-') : ''
+
+    const classes = [
+        'sidebar',
+        !neighbourhood ? 'sidebar-empty' : '',
+        neighbourhood ? 'sidebar-visible' : '',
+    ].join('  ')
+
     if(!neighbourhood){
         return(
-            <div className='sidebar sidebar--empty'>
+            <div className={classes}>
                 <p> Click a neighbourhood on the map</p>
 
             </div>
@@ -13,13 +23,13 @@ function Sidebar({neighbourhood,onClose}){
 
     const slug = neighbourhood.name.toLowerCase().replace(/s+/g,'-')
     return(
-        <div className='sidebar'>
+        <div className={classes}>
             <button className="sidebar_close" onClick={onClose}>X</button>
             <div className='sidebar_color-bar' style={{background : neighbourhood.color}}/>
             <h2 className="sidebar_name">{neighbourhood.name}</h2>
             <p className="sidebar_vibe">{neighbourhood.vibe}</p>
             <Link to={`neighbourhood/${slug}`} className='sidebar_link'>
-                Visit full Profile
+                Visit full Profile 
             </Link>
         </div>
 
